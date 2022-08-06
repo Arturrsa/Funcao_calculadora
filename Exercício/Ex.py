3f6ac883ed5c8ac326601420a155ea7a4f4b69d6
artur henrique <arturr.sanntos@gmail.com>
2022-08-06 17:10:59 -0300

    Modificado

--- Exercício/Ex.py
+++ Exercício/Ex.py
@@ -1,20 +1,50 @@
 
-print("Escolha:\n \n(a) para adição, \n(s) para subtraçção!\n ")
+# funçao +
+
+def adicao(valor_1, valor_2):
+    return valor_1 + valor_2 
+
+# funçao -
+def subtracao(valor_1, valor_2):
+    return valor_1 - valor_2
+
+# funçao *
+def multiplicacao(valor_1, valor_2):
+    return valor_1 * valor_2
+
+# funçao /
+def divisao(valor_1, valor_2):
+    return valor_1 / valor_2
+
+
+print("Escolha:\n \n(a) para adicao, \n(s) para subtracao, \n(m) para multiplicação, \n(d) para divisão!\n ")
 
 
 a = int(input("Digite o primeiro numero: "))
 b = int(input("Digite o segundo numero: "))
 
 escolha = input("Escolha a operação: ")
+    
+
 
 # adição
 if escolha == 'a': 
-    resultado = a + b
+    print(adicao(a, b))
     
 # subtração
-if escolha == 's':  
-    resultado = a - b
+elif escolha == 's':  
+    print(subtracao(a, b))
+   
+# Multiplicação
+elif escolha == 'm':
+    print(multiplicacao(a, b))
     
-
-print(resultado)
-
+# Divisão
+elif escolha == 'd':
+    print(divisao(a, b))
+    
+else:
+     print(0)
+ 
+    
+    
