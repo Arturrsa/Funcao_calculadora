069d8a323601f3c6287ed9c47f7521e28dcca4c0
artur henrique <arturr.sanntos@gmail.com>
2022-08-06 22:09:49 -0300

    Modificado

--- Exercício/Ex.py
+++ Exercício/Ex.py
@@ -17,11 +17,11 @@ def divisao(valor_1, valor_2):
     return valor_1 / valor_2
 
 
-print("Escolha:\n \n(a) para adicao, \n(s) para subtracao, \n(m) para multiplicação, \n(d) para divisão!\n ")
+print("Escolha:\n \n(a) para adicao \n(s) para subtracao \n(m) para multiplicação \n(d) para divisão\n ")
 
 
-a = int(input("Digite o primeiro numero: "))
-b = int(input("Digite o segundo numero: "))
+a = float(input("Digite o primeiro numero: "))
+b = float(input("Digite o segundo numero: "))
 
 escolha = input("Escolha a operação: ")
     
